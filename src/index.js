b1641a9814d127ec36ee8f6c8319b23a54211b59
Radymyr <loboda.radimir@gmail.com>
2023-11-04 20:11:13 +0200

    added telegrf lib

--- src/index.js
+++ src/index.js
@@ -32,11 +32,50 @@ async function main(messages) {
   try {
     data = JSON.parse(fs.readFileSync('message.json', 'utf-8'));
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 
   combinedData = [...data, ...messages, answer];
   fs.writeFileSync('message.json', JSON.stringify(combinedData));
 }
 
-main(messages);
+// main(messages);
+
+bot.on('message', async (ctx) => {
+  console.log(ctx.message);
+
+  try {
+    if (ctx.message.video_note) {
+      ctx.reply(`${ctx.from.first_name}, позже гляну`, {
+        chat_id: ctx.chat.id,
+      });
+      return;
+    }
+
+    if (ctx.message.voice) {
+      ctx.reply(
+        `Прости бро ${ctx.from.first_name}, мне мамка запрещает слушать голосовые`,
+        { chat_id: ctx.chat.id }
+      );
+      return;
+    }
+
+    if (ctx.message.reply_to_message?.from.is_bot) {
+      const originalMessage = ctx.message.reply_to_message;
+
+      ctx.reply('это ответ на сообщение', {
+        reply_to_message_id: originalMessage.message_id,
+      });
+    }
+
+    if (ctx.message.text.includes('@Cheese_GPT_bot')) {
+      ctx.reply(`Hello @${ctx.from.username}, ты обратился к боту через @`, {
+        reply_to_message_id: ctx.message.message_id,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+bot.launch();
