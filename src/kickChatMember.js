119f78120044145225162714c4a9b2bf2d9672a8
Radymyr <loboda.radymyr@gmail.com>
2024-04-28 16:52:11 +0300

    fix empty message

--- src/kickChatMember.js
+++ src/kickChatMember.js
@@ -5,9 +5,14 @@ export async function kickMembers(ctx) {
   try {
     await bot.telegram.kickChatMember(ctx.chat.id, ctx.message.from.id);
 
-    await ctx.reply(`user is kicked`);
+    await ctx.reply(`user: ${ctx.message.from.id} is kicked`);
 
-    console.log(ctx.message.from.id);
+    console.log(
+      'userID:',
+      ctx.message.from.id,
+      'user name:',
+      ctx.from.first_name
+    );
   } catch (error) {
     console.error('Ошибка:', error);
   }
